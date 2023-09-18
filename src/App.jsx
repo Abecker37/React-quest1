d52016edeb72f3e5730c4cde14d37c5072f6d5bb
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-09-18 11:54:09 +0200

    quete 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
-import MyTitle from "./components/MyTitle"
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard"
 import './App.css'
 
 function App() {
   const pokemonList = [
-
-    {
-
-        name: "bulbasaur",
-
-        imgSrc:
-
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-
-    },
-
-    {
-
-        name: "mew",
-
-    },
-
+{
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
 ];
+const [pokemonIndex,setPokemonIndex] = useState(0);
+
+const nextClick = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+
+const decrementClick = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
+
   return (
     <>
       <div>
-        <PokemonCard pokemon = {pokemonList[1]}/>
+        <PokemonCard pokemon = {pokemonList[pokemonIndex]}/>
+        {pokemonIndex > 0 ? <button onClick={decrementClick}>precedent</button> : "" }
+        {pokemonIndex < pokemonList.length - 1 ?  <button onClick={nextClick}>suivant</button> : "" }
       </div>
     </>
   )
