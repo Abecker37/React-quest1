8ddc9fc706649c3737e1c690397a9a043e786765
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-09-18 10:11:38 +0200

    quete 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,31 @@
-function PokemonCard(){
+function PokemonCard() {
+    const pokemon = pokemonList[0]
+
+
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-        <figcaption>bulbasaur</figcaption>
+        {pokemon.imgSrc === undefined ? "???" : <img src={pokemon.imgSrc} alt={pokemon.name} />}
+
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
 }
+
+const pokemonList = [
+
+    {
+
+        name: "bulbasaur",
+
+        imgSrc:
+
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+    },
+
+    {
+
+        name: "mew",
+
+    },
+
+];
 export default PokemonCard
